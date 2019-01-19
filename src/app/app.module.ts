267c247e4284568dd4e14f3eff15fee88aa8f4a7
marcosjrs <marcosjrs80@gmail.com>
2019-01-19 19:59:11 +0100

    Creación de los componentes vacios mediante el CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoFooterComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule,
