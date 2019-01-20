18f66cc45ca9dc9b270ac14c30f939d08d296538
marcosjrs <marcosjrs80@gmail.com>
2019-01-20 13:19:58 +0100

    Render filtros de forma dinamica y establecer valor en store, on click

--- src/app/todo/todo-footer/todo-footer.component.ts
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "../reducer/app.reducers";
+import * as fromFiltro from "../actions/filtro.actions";
+import { SetFiltroAction } from "../actions/filtro.actions";
 
 @Component({
-  selector: 'app-todo-footer',
-  templateUrl: './todo-footer.component.html',
+  selector: "app-todo-footer",
+  templateUrl: "./todo-footer.component.html",
   styles: []
 })
 export class TodoFooterComponent implements OnInit {
+  //fromFiltro.filtrosValidos unos string determinados 'todos' | 'completados' | 'pendientes'
+  filtrosValidos: fromFiltro.filtrosValidos[] = [
+    "todos",
+    "completados",
+    "pendientes"
+  ];
+  filtroActual: fromFiltro.filtrosValidos;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.store
+      .select("filtro")
+      .subscribe(filtro => (this.filtroActual = filtro));
   }
 
+  setFiltro(filtro: fromFiltro.filtrosValidos) {
+    this.store.dispatch(new SetFiltroAction(filtro));
+  }
 }
