fc96fa665c912d7476fc659c0e7723fbe598ed77
marcosjrs <marcosjrs80@gmail.com>
2019-01-20 10:59:19 +0100

    Listado de Tareas. Tareas representando su estado Falta llevarlo a store

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
+import { Todo } from "../model/todo.model";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
   styles: []
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  chkField: FormControl;
+  txtInput: FormControl;
+  editando: boolean;
+  @ViewChild("inputEdit") inputEdit: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.chkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+    this.chkField.valueChanges.subscribe(
+      (value)=>console.log(value)
+    );
   }
 
+  editar() {
+    this.editando = !this.editando;
+    setTimeout(() => {
+      this.inputEdit.nativeElement.select();
+    }, 0);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+  }
 }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "../reducer/app.reducers";
+import { Todo } from "../model/todo.model";
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
+  selector: "app-todo-list",
+  templateUrl: "./todo-list.component.html",
   styles: []
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<AppState>) {
+    this.store.select("todos").subscribe(todos => (this.todos = todos));
   }
 
+  ngOnInit() {}
 }
